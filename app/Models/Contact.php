404e2f40cd0c5d129612af2f8127ee95b462c3e8
Yordism <yordisponcealderete@gmail.com>
2025-04-30 02:18:17 -0400

    Update

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -17,7 +17,7 @@ class Contact extends Model
         $speciality = request()->input('speciality');
         $phone = request()->input('phone');
         $file = request()->file('cv');
-        Notification::route('mail', 'yordisponcealderete@gmail.com')
+        Notification::route('mail', 'administracion@grupoestucalia.com')
             ->notify(new DynamicNotification([
                 'subject' => "Nuevo mensaje de contacto de {$this->name}",
                 'message' => [
