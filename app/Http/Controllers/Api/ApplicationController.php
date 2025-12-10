50f719c1f735b5f5d37aef9d32b1bbd5bbb7a4db
Yordism <yordisponcealderete@gmail.com>
2025-12-10 00:49:10 -0500

    Update

--- app/Http/Controllers/Api/ApplicationController.php
+++ app/Http/Controllers/Api/ApplicationController.php
@@ -39,7 +39,7 @@ class ApplicationController extends Controller
     {
         $application = Application::with([
             'categories' => function ($query) {
-                $query->select('categories.id', 'categories.name', 'categories.slug', 'categories.image', 'categories.short_description_en', 'categories.short_description_es', 'categories.short_description_fr')
+                $query
                     ->where('is_active', true)
                     ->orderBy('categories.order');
             }
